99a741dfd970624ac0be99832e584e3dd844b7cc
raffapen <rafi.einstein@gmail.com>
2025-04-16 09:52:55 +0300

    Jasmine and getnode updates

--- jasmine/index.ts
+++ jasmine/index.ts
@@ -1,2 +1,19 @@
 
+import os from 'os';
+import path from 'path';
+
 export * as text from "./text";
+
+export const CLASSICO = path.resolve(`${import.meta.dir}/..`);
+
+let _pre_imports_dirname = "";
+
+export function _imports() {
+  const home = os.homedir();
+  _pre_imports_dirname = process.cwd();
+  process.chdir(`${home}/.jasmine`);
+}
+
+export function _end_imports() {
+  process.chdir(_pre_imports_dirname);
+}

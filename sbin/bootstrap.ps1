d339187547208dde999bbe77aee0d4be71845a0e
raffapen <rafi.einstein@gmail.com>
2025-09-19 07:17:02 +0300

    bootstrap.ps1: now includes getmsys2 and classico setup

--- sbin/bootstrap.ps1
+++ sbin/bootstrap.ps1
@@ -2,7 +2,7 @@
 function install-classico {
 	$classico = "$env:LOCALAPPDATA\FormalismLab\classico"
 	if (Test-Path -Path $classico) {
-		Write-Output "Classico installed in $classico"
+		Write-Output "Classico found in $classico: not downloading"
 		return
 	}
 
@@ -12,11 +12,18 @@ function install-classico {
 	try {
 		$tmpdir = New-Item -ItemType Directory -Path (Join-Path -Path $env:TEMP -ChildPath ([System.Guid]::NewGuid().ToString()))
 		cd $tmpdir
+		Write-Output "Downloading Classico ..."
 		irm -O classico-master.zip https://github.com/formalism-labs/classico/archive/refs/heads/master.zip
 		Expand-Archive -Path classico-master.zip -DestinationPath . *>$null
 		New-Item -ItemType Directory -Path "$env:LOCALAPPDATA\FormalismLab" -Force
 		Move-Item -Path classico-master -Destination $classico
-		Write-Output "Classico installed in $classico"
+		Write-Output "Classico downloaded into $classico"
+		cd $env:LocalAppData\Local\FormalismLab\classico\bin
+		Write-Output "Installing MSYS2 ..."
+		& .\getmsys2.ps1
+		Write-Output "Setting up Classico ..."
+		& c:\msys64\usr\bin\bash.exe -l -c "~/.local/classico/sbin/setup"
+		Write-Output "Done."
 	} catch {
 		pop-location
 		Write-Error "Error downloading Classico"

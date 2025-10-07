ed1bf31f0ac6e180278f04d60c4042c159c4367d
raffapen <rafi.einstein@gmail.com>
2025-10-07 19:00:22 +0300

    posh: added bash() and with-env

--- posh/utils.ps1
+++ posh/utils.ps1
@@ -31,3 +31,50 @@ function is_command([string] $cmd) {
 }
 
 #----------------------------------------------------------------------------------------------
+
+function With-Env {
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [hashtable] $vars,
+        [Parameter(Mandatory, Position = 1)]
+        [ScriptBlock] $script
+    )
+
+    # Save values
+    $old = @{}
+    foreach ($k in $vars.Keys) {
+        $old[$k] = (Get-Item "Env:$k" -ErrorAction SilentlyContinue).Value
+        Set-Item "Env:$k" $vars[$k]
+    }
+
+    try {
+		& $script
+		$exitCode = $LASTEXITCODE
+        if ($exitCode -ne 0) {
+            throw "Command failed with exit code $exitCode"
+        }
+    } finally {
+        # Restore values
+        foreach ($k in $vars.Keys) {
+            if ($null -eq $old[$k]) {
+                Remove-Item "Env:$k" -ErrorAction SilentlyContinue
+            } else {
+                Set-Item "Env:$k" $old[$k]
+            }
+        }
+    }
+}
+
+#----------------------------------------------------------------------------------------------
+
+function bash([string] $cmd) {
+    $bash = "C:\msys64\usr\bin\bash.exe"
+	with-env @{ MSYSTEM = "MINGW64"; MSYS = $env:MSYS + ",disable_pcon" } {
+		& $bash -l -c "$cmd"
+	}
+    if ($LASTEXITCODE -ne 0) {
+        throw "'$cmd' failed with exit code $LASTEXITCODE"
+    }
+}
+
+#----------------------------------------------------------------------------------------------

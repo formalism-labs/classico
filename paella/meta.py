428be9e642bff59b6d27e7e213c757dae4df494c
raffapen <rafi.einstein@gmail.com>
2025-10-07 13:55:16 +0300

    paella/meta: Lazy class

--- paella/meta.py
+++ paella/meta.py
@@ -3,3 +3,13 @@ import inspect, functools
 
 def extract_args(func):
     return list(inspect.signature(func).parameters.values())
+
+class Lazy:
+    def __init__(self, fn):
+        self._fn = fn
+        self._obj = None
+
+    def __getattr__(self, name):
+        if self._obj is None:
+            self._obj = self._fn()
+        return getattr(self._ensure(), name)

c188c9df2976e8b8e44eaadfc3ac35051e56fc58
raffapen <rafi.einstein@gmail.com>
2025-10-11 10:26:45 +0300

    posh: windows-specific code

--- posh/defs.ps1
+++ posh/defs.ps1
@@ -1,8 +1,12 @@
 
 . $PSScriptRoot\types.ps1
+. $PSScriptRoot\commands.ps1
 . $PSScriptRoot\utils.ps1
 . $PSScriptRoot\files.ps1
 . $PSScriptRoot\time.ps1
 . $PSScriptRoot\versions.ps1
 . $PSScriptRoot\class1.ps1
-. $PSScriptRoot\firewall.ps1
+
+if ($PSVersionTable.OS.OSType -eq "Win32NT") {
+	. $PSScriptRoot\firewall.ps1
+}

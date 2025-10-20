bc5262949c0bb32903d225d984373006c117c72e
raffapen <rafi.einstein@gmail.com>
2025-10-20 09:17:40 +0300

    jasmine: fixed home dir path in windows

--- jasmine/index.ts
+++ jasmine/index.ts
@@ -8,8 +8,15 @@ export const CLASSICO = path.resolve(`${import.meta.dir}/..`);
 
 let _pre_imports_dirname = "";
 
+function ux_homedir() {
+	if (process.platform === 'win32') {
+		return "c:/msys64/home/" + os.userInfo()["username"].toLowerCase();
+	}
+	return os.homedir();
+}
+
 export function _imports() {
-  const home = os.homedir();
+  const home = ux_homedir();
   _pre_imports_dirname = process.cwd();
   process.chdir(`${home}/.jasmine`);
 }
